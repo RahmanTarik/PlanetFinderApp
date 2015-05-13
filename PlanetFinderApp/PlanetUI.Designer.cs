e52192eb687db6a595c94a4c19ab15ff7d33bb94
Rahman Tarik <tarikseu@gamil.com>
2015-05-13 20:02:04 +0600

    condition combobox

---
+++ PlanetFinderApp/PlanetUI.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace PlanetFinderApp
+{
+    partial class PlanetUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.planetComboBox = new System.Windows.Forms.ComboBox();
+            this.showButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // planetComboBox
+            // 
+            this.planetComboBox.FormattingEnabled = true;
+            this.planetComboBox.Items.AddRange(new object[] {
+            "",
+            "Mercury",
+            "Venus",
+            "Earth",
+            "Mars",
+            "Jupiter",
+            "Saturn",
+            "Uranus",
+            "Neptune"});
+            this.planetComboBox.Location = new System.Drawing.Point(126, 32);
+            this.planetComboBox.Name = "planetComboBox";
+            this.planetComboBox.Size = new System.Drawing.Size(121, 24);
+            this.planetComboBox.TabIndex = 0;
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(172, 78);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 1;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Planet Name";
+            // 
+            // PlanetUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(277, 257);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.planetComboBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "PlanetUI";
+            this.Text = "PlanetUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox planetComboBox;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PlanetFinderApp/PlanetUI.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlanetFinderApp
+{
+    public partial class PlanetUI : Form
+    {
+        public PlanetUI()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            string planetName = planetComboBox.SelectedItem.ToString();
+
+            if (planetName == "Mercury")
+            {
+                MessageBox.Show("It is in no " + planetComboBox.SelectedIndex + " position");
+            }
+            else if (planetName == "Venus")
+            {
+                MessageBox.Show(planetName + " is in no " + planetComboBox.SelectedIndex + " position");
+            }
+            else if (planetName == "Earth")
+            {
+                MessageBox.Show(planetName + " is in no " + planetComboBox.SelectedIndex + " position");
+            }
+            else if (planetName == "Mars")
+            {
+                MessageBox.Show(planetName + " is in no " + planetComboBox.SelectedIndex + " position");
+            }
+            else if (planetName == "Jupiter")
+            {
+                MessageBox.Show(planetName + " is in no " + planetComboBox.SelectedIndex + " position");
+            }
+            else if (planetName == "Saturn")
+            {
+                MessageBox.Show(planetName + " is in no " + planetComboBox.SelectedIndex + " position");
+            }
+            else if (planetName == "Uranus")
+            {
+                MessageBox.Show(planetName + " is in no " + planetComboBox.SelectedIndex + " position");
+            }
+            else if (planetName == "Neptune")
+            {
+                MessageBox.Show(planetName + " is in no " + planetComboBox.SelectedIndex + " position");
+            }
+            else
+            {
+                MessageBox.Show("I don’t know");
+            }
+        }
+    }
+}
